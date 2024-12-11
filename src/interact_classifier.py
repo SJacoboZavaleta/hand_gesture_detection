1602767b01acae6e0fd2c1c0b9e6b680bb7da30a
SJacoboZavaleta <sergioaugustoantenor.jacobo@alumnos.uc3m.es>
2024-12-11 15:29:09 +0100

    presentation project day

--- src/interact_classifier.py
+++ src/interact_classifier.py
@@ -46,7 +46,9 @@ class HandGestureRecognizer:
             image_size = (384, 384) if self.model_type == 'efficientnet' else (224, 224)
             self.transform = transforms.Compose([
                 transforms.Resize(image_size),
-                transforms.ToTensor()
+                transforms.ToTensor(),
+                transforms.Normalize(mean=[0.485, 0.456, 0.406],
+                        std=[0.229, 0.224, 0.225])
             ])
 
     def _load_model(self, model_path):
@@ -309,3 +311,7 @@ def main():
 
 if __name__ == "__main__":
     main()
+
+
+# Run
+# python src/interact_classifier.py 1
